5cbf81c134f098679cfa4f10dbf3c4709d3b48c9
ErnoW <ErnoW@users.noreply.github.com>
2022-12-13 19:33:44 +0000

    build: new release [skip ci]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIRES = [
 
 setup(
     name="moralis",
-    version="0.1.7",
+    version="0.1.8",
     description="The official Moralis Python SDK",
     keywords=["Moralis", "Moralis SDK", "Moralis Python SDK", "Web3", "Crypto", "Ethereum", "Avalanche",
               "Blockchain", "Contracts", "Eth", "Evm", "Fantom", "Nft", "Dapps", "Binance", "Solana"],
--- src/moralis/__init__.py
+++ src/moralis/__init__.py
@@ -1,3 +1,3 @@
 from .evm_api import *
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
