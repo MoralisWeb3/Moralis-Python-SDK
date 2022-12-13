45781ee3c4bbbf7c1e6cb07f9333ebdfa8a16374
Erno <erno@ivan.academy>
2022-12-14 02:03:55 +0700

    ci: test publish script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
+#  Create build artifacts, required for CI and manual publishing
 rm -rf dist
 rm -rf build
 python3 setup.py sdist bdist_wheel
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,1 +1,2 @@
+#  Use for manual publishing (CI is using a github action)
 twine upload dist/* --verbose
