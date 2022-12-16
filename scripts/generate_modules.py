1ee7ff0413625cd1474941010e55fd12b9c6e48e
Erno <erno@ivan.academy>
2022-12-16 14:39:58 +0700

    fix: platform version

--- scripts/generate_modules.py
+++ scripts/generate_modules.py
@@ -94,8 +94,8 @@ def get_common_headers():
     """Get the headers for the requests."""
     return {
         "x-moralis-platform": 'Python SDK',
+        "x-moralis-platform-version": __version__,
         "x-moralis-build-target": 'python',
-        "x-moralis-build-target": __version__,
     }
 
 '''
